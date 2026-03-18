8aed6894424eafd0205d22f734e437eebe518028
Berend Vandenbussche <berend@inuits.eu>
2026-03-18 10:45:03 +0100

    Fix: sync type-defs to modules

--- client-frontend/sync-type-defs.sh
+++ client-frontend/sync-type-defs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 src="./inuits-dams-graphql-service/generated-types/type-defs.ts"
 for d in ./inuits-dams-graphql-service/modules/*/; do
-  [ -d "$d" ] && cp "$src" "${d}generated-types/type-defs.ts" && echo "Synced type-defs.ts to $d"
+  [ -d "$d" ] && cp "$src" "${d}/type-defs.ts" && echo "Synced type-defs.ts to $d"
 done
