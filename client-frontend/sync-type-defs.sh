39a1a771cc2e33b854be0268ec6767a32c21ec81
Berend Vandenbussche <berend@inuits.eu>
2026-03-24 11:42:59 +0100

    Fix: sync type-defs to generated-types folder

--- client-frontend/sync-type-defs.sh
+++ client-frontend/sync-type-defs.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 src="./inuits-dams-graphql-service/generated-types/type-defs.ts"
 for d in ./inuits-dams-graphql-service/modules/*/; do
-  [ -d "$d" ] && cp "$src" "${d}/type-defs.ts" && echo "Synced type-defs.ts to $d"
+  if [ -d "$d" ]; then
+    mkdir -p "${d}/generated-types"
+    cp "$src" "${d}/generated-types/type-defs.ts" && echo "Synced type-defs.ts to ${d}generated-types/"
+  fi
 done
